d122008450fdaf7772d7f4d9f4042f32c9592770
Ehouarn-Nouelig Sioly <eh.dev.sioly@gmail.com>
2025-07-01 15:58:29 +0200

    Adding the first image from the database ; Added the 'figureWork' function ; Moved function from <body> tag to <.gallery> tag

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,16 +1,28 @@
 getWorks();
 
 async function getWorks () {
-  const url = "http://localhost:5678/api/works";
+  const url = 'http://localhost:5678/api/works';
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const json = await response.json();
-    console.log(json, "test123");
+    const work = await response.json();
+    console.log(work, "test123");
+    figureWork(work);
   } catch (error) {
     console.error(error.message);
   }
 }
+
+function figureWork(data) {
+    
+    const gallery = document.querySelector(".gallery");
+
+    const figure = document.createElement("figure");
+    figure.innerHTML = `<img src=${data[0].imageUrl} alt=${data[0].title}> <figcaption>${data[0].title}</figcaption>`;
+
+    gallery.appendChild(figure);
+}
+
