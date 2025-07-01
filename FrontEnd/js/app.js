4f99c844fa808522b3bce03279a75b1c0a754559
Ehouarn-Nouelig Sioly <eh.dev.sioly@gmail.com>
2025-07-01 14:10:26 +0200

    Add app.js ; Delete all figure in html ; Creation of a fetch api for retrieving works

---
+++ FrontEnd/js/app.js
@@ -0,0 +1,16 @@
+getWorks();
+
+async function getWorks () {
+  const url = "http://localhost:5678/api/works";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json, "test123");
+  } catch (error) {
+    console.error(error.message);
+  }
+}
