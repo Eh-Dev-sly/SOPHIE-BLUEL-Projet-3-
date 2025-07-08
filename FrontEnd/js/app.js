a3c9f886d9f705bfd58efab48863133327ee010e
Ehouarn-Nouelig Sioly <eh.dev.sioly@gmail.com>
2025-07-08 10:03:21 +0200

    Add filters with categories

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,4 +1,6 @@
+// Function to get works of the DataBase and API
 getWorks();
+let allWorks = [];
 
 async function getWorks () {
   const url = 'http://localhost:5678/api/works';
@@ -8,10 +10,10 @@ async function getWorks () {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const works = await response.json();
-    console.log(works);
-    for (let i = 0; i < works.length ; i++) {
-        figureWork(works[i]);
+    allWorks = await response.json(); // <- on stocke les œuvres dans la variable globale
+    console.log(allWorks);
+    for (let i = 0; i < allWorks.length ; i++) {
+        figureWork(allWorks[i]);
     }
   } catch (error) {
     console.error(error.message);
@@ -19,11 +21,66 @@ async function getWorks () {
 }
 
 function figureWork(data) {
-    const gallery = document.querySelector(".gallery");
-
-    const figure = document.createElement("figure");
-    figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}> <figcaption>${data.title}</figcaption>`;
-
-    gallery.appendChild(figure);
+  const gallery = document.querySelector(".gallery"); // Sélection de la galerie
+  
+  const figure = document.createElement("figure"); // Création d'une figure pour chaque œuvre
+  figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}> <figcaption>${data.title}</figcaption>`; // Ajout de l'image et du titre de l'œuvre
+  
+  gallery.appendChild(figure); // Ajout de la figure à la galerie
 }
 
+async function getCategories() {
+  const url = "http://localhost:5678/api/categories";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+getCategories();
+
+const btnAll = document.querySelector('.btn_all');
+
+btnAll.addEventListener('click', function () {
+  const objets = allWorks;
+
+  document.querySelector(".gallery").innerHTML = ""; // vide la galerie
+
+  objets.forEach(figureWork); // génère uniquement les objets
+});
+
+const btnObjects = document.querySelector('.btn_objects');
+
+btnObjects.addEventListener('click', function () {
+  const objets = allWorks.filter(work => work.categoryId === 1);
+
+  document.querySelector(".gallery").innerHTML = ""; // vide la galerie
+
+  objets.forEach(figureWork); // génère uniquement les objets
+});
+
+const btnAppartements = document.querySelector('.btn_appartements');
+
+btnAppartements.addEventListener('click', function () {
+  const objets = allWorks.filter(work => work.categoryId === 2);
+
+  document.querySelector(".gallery").innerHTML = ""; // vide la galerie
+
+  objets.forEach(figureWork); // génère uniquement les objets
+});
+
+const btnHotelsRestau = document.querySelector('.btn_hotels-restaurants');
+
+btnHotelsRestau.addEventListener('click', function () {
+  const objets = allWorks.filter(work => work.categoryId === 3);
+
+  document.querySelector(".gallery").innerHTML = ""; // vide la galerie
+
+  objets.forEach(figureWork); // génère uniquement les objets
+});
